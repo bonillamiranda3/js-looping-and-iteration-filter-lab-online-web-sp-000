3477f7282d058f3c423b1beb35a24ea3b4de59de
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-02-03 05:57:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+
+function findMatching(driversArray, string) {
+  return driversArray.filter(function(driverElement) {
+    return driverElement.toLowerCase() === string.toLowerCase();
+  });
+}
+
+fuction fuzzyMatch(driversArray, string) {
+  return driversArray.filter (function (driverElement){
+    return driverElement.slice(0, string.length) === string
+})
+}
